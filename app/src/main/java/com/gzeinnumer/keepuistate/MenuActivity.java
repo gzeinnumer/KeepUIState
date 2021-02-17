3d7a460b90881435668f4ec9c8affeffe124ef71
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-02-18 01:08:37 +0700

    Support SDK 16

---
+++ app/src/main/java/com/gzeinnumer/keepuistate/MenuActivity.java
@@ -0,0 +1,48 @@
+package com.gzeinnumer.keepuistate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gzeinnumer.mylibsavedinstancestate.fragment.FragmentTestActivity;
+import com.gzeinnumer.mylibsavedinstancestate.image.ImageActivity;
+import com.gzeinnumer.mylibsavedinstancestate.recyclerView.RecyclerViewActivity;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        findViewById(R.id.btn_example_activity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+        findViewById(R.id.btn_example_fragment).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), FragmentTestActivity.class));
+            }
+        });
+
+        findViewById(R.id.btn_example_imageview).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ImageActivity.class));
+            }
+        });
+
+        findViewById(R.id.btn_example_recyclerview).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RecyclerViewActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/gzeinnumer/keepuistate/fragment/HomeFragment.java
@@ -0,0 +1,86 @@
+package com.gzeinnumer.keepuistate.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.gzeinnumer.keepuistate.MenuActivity;
+import com.gzeinnumer.keepuistate.databinding.FragmentHomeBinding;
+import com.gzeinnumer.keepuistate.utils.CustomToastDown;
+import com.gzeinnumer.keepuistate.utils.CustomToastUp;
+import com.gzeinnumer.kus.StateUI;
+import com.gzeinnumer.kus.StateUIBuilder;
+
+public class HomeFragment extends Fragment {
+    public static final String TAG = "State_UI";
+
+    private FragmentHomeBinding binding;
+
+    private StateUI stateUI;
+
+    public HomeFragment() {
+    }
+
+    public static HomeFragment newInstance() {
+        return new HomeFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        stateUI = StateUIBuilder.Build(HomeFragment.class, requireContext());
+
+        binding.btnClearBack.setOnClickListener(v -> {
+            stateUI.clearState();
+            startActivity(new Intent(requireContext(), MenuActivity.class));
+        });
+
+        binding.btnSaveBack.setOnClickListener(v -> {
+            startActivity(new Intent(requireContext(), MenuActivity.class));
+            requireActivity().finish();
+        });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        new CustomToastUp(requireContext(), "Fragment_onPause\nData Save To State");
+
+        stateUI.addView("binding.edUsername", binding.edUsername.getText().toString());
+        stateUI.addView("binding.edPass", binding.edPass.getText().toString());
+        stateUI.saveState();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (stateUI.getState()) {
+            new CustomToastDown(requireContext(), "Fragment_onResume\nData Loaded From State");
+
+            String userName = stateUI.getValue("binding.edUsername");
+            binding.edUsername.setText(userName);
+            String pass = stateUI.getValue("binding.edPass");
+            binding.edPass.setText(pass);
+        }
+    }
+
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//        stateUI.clearState();
+//    }
+}
